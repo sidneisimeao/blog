504dc35a52a03d42a33448eefa6800ffd5189c09
sidneisimeao <sidneisimmon@gmail.com>
2018-10-05 22:27:25 -0300

    Correção Template

--- static.config.js
+++ static.config.js
@@ -2,25 +2,27 @@ import React from "react";
 import { reloadRoutes } from 'react-static/node'
 import jdown from 'jdown'
 import chokidar from 'chokidar'
-import axios from 'axios'
-import ExtractTextPlugin from 'extract-text-webpack-plugin'
-
-const LoadingContext = React.createContext({
-  loading: false,
-  message: null,
-  showLoading: message => { },
-  hideLoading: () => { }
-})
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 chokidar.watch('content').on('all', () => reloadRoutes())
 
 export default {
+  Document: ({ Html, Head, Body, children, siteData, renderMeta }) => (
+    <Html lang="pt-br">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{"<SidneiSimmon/>"}</title>
+      </Head>
+      <Body>{children}</Body>
+    </Html>
+  ),
   getSiteData: () => ({
     title: 'React Static',
   }),
   getRoutes: async () => {
     // const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    const { posts } = await jdown('content')   
+    const { posts } = await jdown('content')
     return [
       {
         path: '/',
